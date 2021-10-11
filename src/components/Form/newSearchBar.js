21b6914ea534d04d137ef303fcd4b2b9b665db84
Raju Moni Borah <raju42fun@gmail.com>
2021-10-11 21:10:26 +0530

    completed

--- src/components/Form/newSearchBar.js
+++ src/components/Form/newSearchBar.js
@@ -1,48 +1,27 @@
-import React, { useState,useCallback} from "react";
+import React,{useState} from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import axios from "axios";
 import Container from "@mui/material/Container";
-import debounce from 'lodash.debounce';
-const { REACT_APP_PIXABAY_KEY } = process.env;
 export default function NewSearchBar(props) {
-  let [tags, setTags] = useState([""]);
-  const [searchValue,setSearchValue]=useState(props.searchValue);
-  const suggestionHandler = async (search) => {
-    if (search) {
-      const response = await axios.get(
-        `https://pixabay.com/api/?key=${REACT_APP_PIXABAY_KEY}&q=${search}}&image_type=photo&pretty=true`
-      );
-      if (response.data.hits.length > 0) {
-        let suggestion= response.data.hits.map((el)=>{
-            return el.tags.split(",");
-        });
-        suggestion = suggestion.flat();
-        suggestion =  [...new Set(suggestion)];
-        setTags(suggestion);
-        }
-     
-    }
-  };
-  const debouncedChangeHandler = useCallback(
-    debounce((searchValue)=>props.onSearch(searchValue), 500)
-  , [searchValue]);
   return (
     <Container style={{ display: "flex", justifyContent: "center" }}>
         <Autocomplete
           disablePortal
+          onChange={(event,newValue)=>{
+            props.onSearch(newValue);
+          }}
           id="combo-box-demo"
-          options={tags}
+          options={props.tags}
           sx={{ width: 300 }}
           renderInput={(params) => (
             <TextField
               {...params}
               required
-              onChange={(event) => {
-                setSearchValue(event.target.value);
-                props.onChangeSearchValue(event.target.value);
-                suggestionHandler(event.target.value);
-                debouncedChangeHandler();
+              value={props.searchValue}
+              onChange={(event)=>{
+                let search = event.target.value;
+                props.onSearch(search);
+
               }}
             />
           )}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect ,useCallback} from "react";
 import SimpleContainer from "../components/UI/SimpleContainer";
 import StandardImageList from "../components/List/StandardImageList";
 import Paginate from "../components/Paginate/Paginate";
 import axios from "axios";
 import NewSearchBar from "../components/Form/newSearchBar";
-
+import debounce from 'lodash.debounce';
+import CircularProgress from '@mui/material/CircularProgress';
 const { REACT_APP_PIXABAY_KEY } = process.env;
 
 const HomePage = () => {
@@ -12,10 +13,23 @@ const HomePage = () => {
   const [totalPages, setTotalPages] = useState(1);
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("Car");
-  const searchHandlder = async () => {
+  const [tags, setTags] = useState([""]);
+  const [isLoading,setIsLoading] =useState(true);
+
+  const searchHandlder = async (search="car") => {
+    setIsLoading(true);
     const response = await axios.get(
       `https://pixabay.com/api/?key=${REACT_APP_PIXABAY_KEY}&q=${search}&image_type=photo&pretty=true&per_page=10&page=1}`
     );
+    setIsLoading(false);
+    if (response.data.hits.length > 0) {
+      let suggestion= response.data.hits.map((el)=>{
+          return el.tags.split(",");
+      });
+      suggestion = suggestion.flat();
+      suggestion =  [...new Set(suggestion)];
+      setTags(suggestion);
+      }
     setPage(1);
     setImageList(response.data.hits);
     setTotalPages(Math.floor(response.data.totalHits / 10));
@@ -25,7 +39,9 @@ const HomePage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
-
+  const debouncedChangeHandler = useCallback(
+    debounce(searchHandlder, 0)
+  , [search]);
   return (
     <SimpleContainer>
       <h1 style={{ textAlign: "center" }}>PixaBay Image Gallery</h1>
@@ -33,10 +49,12 @@ const HomePage = () => {
       <NewSearchBar
         searchValue={search}
         onChangeSearchValue={setSearch}
-        onSearch={searchHandlder}
-      ></NewSearchBar>
+        onSearch={debouncedChangeHandler}
+        tags={tags}
+      />
       {/* {imageList.length} */}
-      <StandardImageList images={imageList}></StandardImageList>
+      {isLoading && <div style={{display:"flex",justifyContent:"center", padding:"20px"}}><CircularProgress disableShrink /></div>}
+      {!isLoading &&  <StandardImageList images={imageList}/>}
       <Paginate
         searchValue={search}
         totalPages={totalPages}
